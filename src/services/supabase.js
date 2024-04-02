fefa9e91fd25a4274c5f61f4352a1be3089cff5a
AndSim <siminiucandrew@gmail.com>
2024-04-02 13:20:24 +0300

    feat: in supabase implement the cabins guests and settings tables. in booking table connect with cabins guests id table. Add access data from Supabase on app (Cabins). And add row-level security

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://imilfagteomkpujptirv.supabase.co';
+const supabaseKey =
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltaWxmYWd0ZW9ta3B1anB0aXJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE3OTczMjQsImV4cCI6MjAyNzM3MzMyNH0.cyCkQEB5qpK3hMdSunoLaPyCwTBrdZTYd5K3jMu0Ppk';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
