8e383cbf636da9c6cb01656d05f32e088333412d
AndSim <siminiucandrew@gmail.com>
2024-04-02 14:09:28 +0300

    feat: set up two storage buckets to upload large files (avatars, cabin-images) display in (Cabins)

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -12,6 +12,11 @@ function Cabins() {
 		<Row type="horizontal">
 			<Heading as="h1">All cabins</Heading>
 			<p>TEST</p>
+
+			<img
+				src="https://imilfagteomkpujptirv.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"
+				alt="cabin-001"
+			/>
 		</Row>
 	);
 }
