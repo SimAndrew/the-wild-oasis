6b67394b36c931bbc6e150db07e83467d8fe3010
AndSim <siminiucandrew@gmail.com>
2024-04-05 15:13:22 +0300

    feat: (CabinRow) display notifications with `react-hot-toast.com` lib

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,4 +1,5 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
 import styled from 'styled-components';
 import { formatCurrency } from '../../utils/helpers.js';
 import { deleteCabin } from '../../services/apiCabins.js';
@@ -57,13 +58,13 @@ function CabinRow({ cabin }) {
 	const { isLoading: isDeleting, mutate } = useMutation({
 		mutationFn: deleteCabin,
 		onSuccess: () => {
-			alert('Cabin successfully deleted!');
+			toast.success('Cabin successfully deleted!');
 
 			queryClient.invalidateQueries({
 				queryKey: ['cabins'],
 			});
 		},
-		onError: (err) => alert(err.message),
+		onError: (err) => toast.error(err.message),
 	});
 
 	return (
