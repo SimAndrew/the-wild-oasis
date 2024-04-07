3019059ac93311b356d62a48bafaea6337872d67
AndSim <siminiucandrew@gmail.com>
2024-04-07 13:59:55 +0300

    feat: create a new cabin func (CreateCabinForm) `react-hook-form.com` lib. Without adding a photo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'react-hot-toast';
+import { StyleSheetManager } from 'styled-components';
 
 import GlobalStyles from './styles/GlobalStyles.jsx';
 import Dashboard from './pages/Dashboard.jsx';
@@ -24,48 +25,52 @@ const queryClient = new QueryClient({
 
 function App() {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<ReactQueryDevtools initialIsOpen={false} />
+		<StyleSheetManager shouldForwardProp={(prop) => prop !== 'variation'}>
+			{
+				<QueryClientProvider client={queryClient}>
+					<ReactQueryDevtools initialIsOpen={false} />
 
-			<GlobalStyles />
-			<BrowserRouter>
-				<Routes>
-					<Route element={<AppLayout />}>
-						<Route index element={<Navigate replace to="dashboard" />} />
-						<Route path="dashboard" element={<Dashboard />} />
-						<Route path="bookings" element={<Bookings />} />
-						<Route path="cabins" element={<Cabins />} />
-						<Route path="users" element={<Users />} />
-						<Route path="settings" element={<Settings />} />
-						<Route path="account" element={<Account />} />
-					</Route>
+					<GlobalStyles />
+					<BrowserRouter>
+						<Routes>
+							<Route element={<AppLayout />}>
+								<Route index element={<Navigate replace to="dashboard" />} />
+								<Route path="dashboard" element={<Dashboard />} />
+								<Route path="bookings" element={<Bookings />} />
+								<Route path="cabins" element={<Cabins />} />
+								<Route path="users" element={<Users />} />
+								<Route path="settings" element={<Settings />} />
+								<Route path="account" element={<Account />} />
+							</Route>
 
-					<Route path="login" element={<Login />} />
-					<Route path="*" element={<PageNotFound />} />
-				</Routes>
-			</BrowserRouter>
+							<Route path="login" element={<Login />} />
+							<Route path="*" element={<PageNotFound />} />
+						</Routes>
+					</BrowserRouter>
 
-			<Toaster
-				position="top-center"
-				gutter={12}
-				containerStyle={{ margin: '8px' }}
-				toastOptions={{
-					success: {
-						duration: 5000,
-					},
-					error: {
-						duration: 8000,
-					},
-					style: {
-						fontSize: '16px',
-						maxWidth: '500px',
-						padding: '16px 24px',
-						backgroundColor: 'var(--color-grey-0)',
-						color: 'var(--color-grey-700)',
-					},
-				}}
-			/>
-		</QueryClientProvider>
+					<Toaster
+						position="top-center"
+						gutter={12}
+						containerStyle={{ margin: '8px' }}
+						toastOptions={{
+							success: {
+								duration: 5000,
+							},
+							error: {
+								duration: 8000,
+							},
+							style: {
+								fontSize: '16px',
+								maxWidth: '500px',
+								padding: '16px 24px',
+								backgroundColor: 'var(--color-grey-0)',
+								color: 'var(--color-grey-700)',
+							},
+						}}
+					/>
+				</QueryClientProvider>
+			}
+		</StyleSheetManager>
 	);
 }
 
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -4,9 +4,17 @@ export async function getCabins() {
 	const { data, error } = await supabase.from('cabins').select('*');
 
 	if (error) {
-		throw new Error('Cabins could not be loaded!');
+		throw new Error('Cabins could not be Loaded!');
 	}
+	return data;
+}
 
+export async function createCabin(newCabin) {
+	const { data, error } = await supabase.from('cabins').insert([newCabin]);
+
+	if (error) {
+		throw new Error('Cabin could not be Created!');
+	}
 	return data;
 }
 
@@ -14,8 +22,7 @@ export async function deleteCabin(id) {
 	const { data, error } = await supabase.from('cabins').delete().eq('id', id);
 
 	if (error) {
-		throw new Error('Cabin could not be deleted!');
+		throw new Error('Cabin could not be Deleted!');
 	}
-
 	return data;
 }
