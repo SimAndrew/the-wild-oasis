5046e049a5d4b39b2f7f1160991dfaf25d08a106
AndSim <siminiucandrew@gmail.com>
2024-04-12 13:17:59 +0300

    feat: in (CabinTable) add reusable (Table) to compound component

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -6,18 +6,7 @@ import Modal from '../../ui/Modal.jsx';
 import ConfirmDelete from '../../ui/ConfirmDelete.jsx';
 import styled from 'styled-components';
 import { HiPencil, HiSquare2Stack, HiTrash } from 'react-icons/hi2';
-
-const TableRow = styled.div`
-	display: grid;
-	grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-	column-gap: 2.4rem;
-	align-items: center;
-	padding: 1.4rem 2.4rem;
-
-	&:not(:last-child) {
-		border-bottom: 1px solid var(--color-grey-100);
-	}
-`;
+import Table from '../../ui/Table.jsx';
 
 const Img = styled.img`
 	display: block;
@@ -72,7 +61,7 @@ function CabinRow({ cabin }) {
 	}
 
 	return (
-		<TableRow role="row">
+		<Table.Row>
 			<Img src={image} />
 			<Cabin>{name}</Cabin>
 			<div>Fits up to {maxCapacity} guests</div>
@@ -114,7 +103,7 @@ function CabinRow({ cabin }) {
 					</Modal.Window>
 				</Modal>
 			</div>
-		</TableRow>
+		</Table.Row>
 	);
 }
 
