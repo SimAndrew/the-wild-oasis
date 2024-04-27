c42b626976777fb345a0295feaa408de929ea609
AndSim <siminiucandrew@gmail.com>
2024-04-27 13:58:48 +0300

    feat: in (Header) Logout user func (Logout)

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -6,7 +6,6 @@ export async function login({ email, password }) {
 		password,
 	});
 	if (error) throw new Error(error.message);
-
 	return data;
 }
 
@@ -16,6 +15,10 @@ export async function getCurrentUser() {
 
 	const { data, error } = await supabase.auth.getUser();
 	if (error) throw new Error(error.message);
-
 	return data?.user;
+}
+
+export async function logout() {
+	const { error } = await supabase.auth.signOut();
+	if (error) throw new Error(error.message);
 }
