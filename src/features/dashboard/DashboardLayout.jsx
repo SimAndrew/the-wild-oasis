4d6c6fbe2b80ba1dc760415da0ba4d307d0d8174
AndSim <siminiucandrew@gmail.com>
2024-05-02 20:52:06 +0300

    feat: (Dashboard, DashboardLayout) display statistics(Stats, Stat) of: num bookings, recent sales, check-ins, total occupancy rate.

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 import { useRecentBookings } from './useRecentBookings.jsx';
 import Spinner from '../../ui/Spinner.jsx';
 import { useRecentStays } from './useRecentStays.jsx';
+import Stats from './Stats.jsx';
+import { useCabins } from '../cabins/useCabins.js';
 
 const StyledDashboardLayout = styled.div`
 	display: grid;
@@ -12,15 +14,22 @@ const StyledDashboardLayout = styled.div`
 
 function DashboardLayout() {
 	const { bookings, isLoading: isLoading1 } = useRecentBookings();
-	const { isLoading: isLoading2, stays, confirmedStays } = useRecentStays();
+	const { isLoading: isLoading2, numDays, confirmedStays } = useRecentStays();
+	const { cabins, isLoading: isLoading3 } = useCabins();
 
-	if (isLoading1 || isLoading2) return <Spinner />;
+	if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
 
 	console.log(bookings);
 
 	return (
 		<StyledDashboardLayout>
-			<div>Statistics</div>
+			<Stats
+				bookings={bookings}
+				confirmedStays={confirmedStays}
+				numDays={numDays}
+				cabinCount={cabins.length}
+			/>
+
 			<div>Today&apos;s activity</div>
 			<div>Chart stay durations</div>
 			<div>Chart sales</div>
