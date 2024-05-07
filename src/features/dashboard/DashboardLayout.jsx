d9cbd8c4687a9166c48847ddbb29d668bde899af
AndSim <siminiucandrew@gmail.com>
2024-05-07 20:15:57 +0300

    feat: in (DashboardLayout) display pie chart with stay duration summary (DurationChart), add dark mode

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import { useRecentStays } from './useRecentStays.jsx';
 import Stats from './Stats.jsx';
 import { useCabins } from '../cabins/useCabins.js';
 import SalesChart from './SalesChart.jsx';
+import DurationChart from './DurationChart.jsx';
 
 const StyledDashboardLayout = styled.div`
 	display: grid;
@@ -30,7 +31,7 @@ function DashboardLayout() {
 			/>
 
 			<div>Today&apos;s activity</div>
-			<div>Chart stay durations</div>
+			<DurationChart confirmedStays={confirmedStays} />
 			<SalesChart bookings={bookings} numDays={numDays} />
 		</StyledDashboardLayout>
 	);
