00099c5555c7f5ff3059772682159a3fa61b88dc
AndSim <siminiucandrew@gmail.com>
2024-05-08 12:52:35 +0300

    feat: add error boundaries react-error-boundary (ErrorFallback.jsx)

--- src/ui/ErrorFallback.jsx
+++ src/ui/ErrorFallback.jsx
@@ -1,4 +1,7 @@
 import styled from 'styled-components';
+import Heading from './Heading.jsx';
+import GlobalStyles from '../styles/GlobalStyles.jsx';
+import Button from './Button.jsx';
 
 const StyledErrorFallback = styled.main`
 	height: 100vh;
@@ -10,7 +13,6 @@ const StyledErrorFallback = styled.main`
 `;
 
 const Box = styled.div`
-	/* Box */
 	background-color: var(--color-grey-0);
 	border: 1px solid var(--color-grey-100);
 	border-radius: var(--border-radius-md);
@@ -29,3 +31,23 @@ const Box = styled.div`
 		color: var(--color-grey-500);
 	}
 `;
+
+function ErrorFallback({ error, resetErrorBoundary }) {
+	return (
+		<>
+			<GlobalStyles />
+
+			<StyledErrorFallback>
+				<Box>
+					<Heading as="h1">Something went wrong!</Heading>
+					<p>{error.message}</p>
+					<Button size="large" onClick={resetErrorBoundary}>
+						Try again
+					</Button>
+				</Box>
+			</StyledErrorFallback>
+		</>
+	);
+}
+
+export default ErrorFallback;
